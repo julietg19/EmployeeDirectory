df3ba21484b1b6414f90ab75eecbc09c2ff09f2f
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-08-29 11:22:28 -0500

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="bg-info p-5 mb-5">
       <h1>Employee Directory</h1>
-      <p>This is an employee directory.</p>
+      <p>This.</p>
     </div>
   );
 }
