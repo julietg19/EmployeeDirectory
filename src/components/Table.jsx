2a9fefbeedd0e81601036081a6b824fbb4742467
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-09-02 13:07:58 -0500

    search

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,9 +4,38 @@ import Row from "./Row";
 
 export default class Table extends Component {
   state = { employees: data };
+  search = (e) => {
+    const userSearch = e.target.value;
+    const filterSearch = data.filter(
+      (employee) =>
+        employee.toLowerCase().indexOf(userSearch.toLowerCase()) !== -1
+    );
+
+    this.setState({ employees: filterSearch });
+  };
+
   render() {
     return (
       <div>
+        <div className="input-group mb-3 px-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by first name"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+            onChange={(e) => this.search(e)}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="search"
+              id="button-addon2"
+            >
+              Search
+            </button>
+          </div>
+        </div>
         <table className="table table-dark">
           <thead>
             <tr>
