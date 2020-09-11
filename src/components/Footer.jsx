be62192d4200df3f4ac78f43bf44d36ca89b41f1
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-09-10 19:51:59 -0500

    fixheader

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-secondary p-4 mt-4 fixed-bottom">
-      <h6>this is the footer 2020</h6>
+    <div className="bg-secondary p-4 mt-0 fixed-bottom">
+      <h6>2020</h6>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="bg-info p-5 mb-5">
+    <div className="bg-info p-5 mb-1">
       <h1>Employee Directory</h1>
-      <p>This.</p>
     </div>
   );
 }
